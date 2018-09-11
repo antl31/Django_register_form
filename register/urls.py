f03549acd0cdb0f9002afb19a4b7ac7657b7adcd
Anton Lysenko <a.lysenko@speedflow.com>
2018-09-11 12:36:01 +0300

    added page getter for validate your email in db

--- register/urls.py
+++ register/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.new_post, name='new_post'),
+    url(r'^get/', views.index, name='get'),
+
 ]
--- register/views.py
+++ register/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .form import RegisterForm
-
+from .form import RegisterForm, GetterForm
+from .models import Register
 # Create your views here.
+
+
+def index(request):
+    form = GetterForm(data=request.POST)
+    email = 0
+    if form.is_valid():
+        email = form.cleaned_data['email']
+
+    regform = Register.objects.filter(email=email)
+
+    return render(request, "getter.html", {"regform": regform})
 
 
 def new_post(request):
